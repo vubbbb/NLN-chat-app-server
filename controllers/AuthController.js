e2f49242a1825a51531810f69cfab5ec85eb4fd6
Tran Vu Binh <tranb4010@gmail.com>
2024-08-29 22:43:43 +0700

    image upload test

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -119,27 +119,27 @@ export const login = async (req, res, next) => {
 
 export const addProfileImage = async (req, res) => {
   try {
-    console.log(req.file.path);
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: "profiles",
-    });
+    console.log(req.file);
+  //   const result = await cloudinary.uploader.upload(req.file.path, {
+  //     folder: "profiles",
+  //   });
 
-    // Xóa file tạm thời sau khi upload lên Cloudinary
-    unlinkSync(req.file.path);
+  //   // Xóa file tạm thời sau khi upload lên Cloudinary
+  //   unlinkSync(req.file.path);
 
-    const updatedUser = await User.findByIdAndUpdate(
-      req.userID,
-      { image: result.secure_url },
-      { new: true, runValidators: true }
-    );
+  //   const updatedUser = await User.findByIdAndUpdate(
+  //     req.query.userID,
+  //     { image: result.secure_url },
+  //     { new: true, runValidators: true }
+  //   );
 
-    return res.status(201).json({
-      image: updatedUser.image,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json("Internal server error");
-  }
+  //   return res.status(201).json({
+  //     image: updatedUser.image,
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  //   return res.status(500).json("Internal server error");
+  // }
 };
 
 export const removeProfileImage = async (req, res, next) => {
