60d013915e7d7779f038364254a282d0efa36345
Tran Vu Binh <tranb4010@gmail.com>
2024-09-14 13:34:26 +0700

    setup ok

--- socket.js
+++ socket.js
@@ -8,6 +8,31 @@ const socketSetup = (server) => {
   });
 
   const useSocketMap = new Map();
+
+  const disconnect = (socket) => {
+    console.log(`User disconnected: ${socket.id}`);
+    for (const [userID, socketID] of useSocketMap.entries()) {
+      if (socketID === socket.id) {
+        useSocketMap.delete(userID);
+        break;
+      }
+    }
+  };
+
+  io.on("connection", (socket) => {
+    const userID = socket.handshake.query.userID;
+
+    if (userID) {
+      useSocketMap.set(userID, socket.id);
+      console.log(`User connected: ${userID} with socket id: ${socket.id}`);
+    } else {
+      console.log("userID not provided");
+    }
+
+    socket.on("disconnect", () => {
+      disconnect(socket);
+    });
+  });
 };
 
 export default socketSetup;
