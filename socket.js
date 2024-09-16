501f44253f3894e56c0558d1080c69ecbe9e8eb1
Tran Vu Binh <tranb4010@gmail.com>
2024-09-16 17:25:10 +0700

    hhhh

--- socket.js
+++ socket.js
@@ -3,46 +3,51 @@ import { Server as SocketIOServer } from "socket.io";
 const socketSetup = (server) => {
   const io = new SocketIOServer(server, {
     cors: {
-      methods: ["GET", "POST"], // Đảm bảo từ methods đến methods
-      credentials: true,
       origin: "*",
     },
-    allowEIO3: true,
   });
 
-  const useSocketMap = new Map();
+  // const useSocketMap = new Map();
 
-  const disconnect = (socket) => {
-    console.log(`User disconnected: ${socket.id}`);
-    for (const [userID, socketID] of useSocketMap.entries()) {
-      if (socketID === socket.id) {
-        useSocketMap.delete(userID);
-        console.log(`UserID ${userID} removed from map`);
-        break;
-        // hi
-      }
-    }
-  };
+  // const disconnect = (socket) => {
+  //   console.log(`User disconnected: ${socket.id}`);
+  //   for (const [userID, socketID] of useSocketMap.entries()) {
+  //     if (socketID === socket.id) {
+  //       useSocketMap.delete(userID);
+  //       console.log(`UserID ${userID} removed from map`);
+  //       break;
+  //       // hi
+  //     }
+  //   }
+  // };
+
+  // io.on("connection", (socket) => {
+  //   console.log(`User connected:`);
+  //   const userID = socket.handshake.query.userID;
+
+  //   if (userID) {
+  //     useSocketMap.set(userID, socket.id);
+  //     console.log(`User connected: ${userID} with socket id: ${socket.id}`);
+  //   } else {
+  //     console.log("UserID not provided in handshake query");
+  //   }
+
+  //   socket.on("disconnect", () => {
+  //     disconnect(socket);
+  //   });
+  // });
+
+  // // Optional: handle error events
+  // io.on("error", (err) => {
+  //   console.error("Socket.io error:", err);
+  // });
 
   io.on("connection", (socket) => {
-    console.log(`User connected:`);
-    const userID = socket.handshake.query.userID;
-
-    if (userID) {
-      useSocketMap.set(userID, socket.id);
-      console.log(`User connected: ${userID} with socket id: ${socket.id}`);
-    } else {
-      console.log("UserID not provided in handshake query");
-    }
+    console.log(`connect: ${socket.id}`, socket.request.headers);
 
     socket.on("disconnect", () => {
-      disconnect(socket);
+      console.log(`disconnect: ${socket.id}`);
     });
-  });
-
-  // Optional: handle error events
-  io.on("error", (err) => {
-    console.error("Socket.io error:", err);
   });
 };
 
