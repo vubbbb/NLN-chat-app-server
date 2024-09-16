6fbe4af4fb32b78b964b68510c1f50298745cab9
Tran Vu Binh <tranb4010@gmail.com>
2024-09-16 17:09:46 +0700

    hhh

--- socket.js
+++ socket.js
@@ -26,24 +26,25 @@ const socketSetup = (server) => {
   };
 
   io.on("connection", (socket) => {
-    const userID = socket.handshake.query.userID;
+    console.log(`User connected:`);
+    // const userID = socket.handshake.query.userID;
 
-    if (userID) {
-      // Kiểm tra nếu userID đã có kết nối
-      // if (useSocketMap.has(userID)) {
-      //   console.log(
-      //     `UserID ${userID} is already connected with socket id: ${useSocketMap.get(
-      //       userID
-      //     )}`
-      //   );
-      // } else {
-      // Nếu chưa có kết nối thì thêm mới
-      useSocketMap.set(userID, socket.id);
-      console.log(`User connected: ${userID} with socket id: ${socket.id}`);
-      // }
-    } else {
-      console.log("UserID not provided in handshake query");
-    }
+    // if (userID) {
+    //   // Kiểm tra nếu userID đã có kết nối
+    //   // if (useSocketMap.has(userID)) {
+    //   //   console.log(
+    //   //     `UserID ${userID} is already connected with socket id: ${useSocketMap.get(
+    //   //       userID
+    //   //     )}`
+    //   //   );
+    //   // } else {
+    //   // Nếu chưa có kết nối thì thêm mới
+    //   useSocketMap.set(userID, socket.id);
+    //   console.log(`User connected: ${userID} with socket id: ${socket.id}`);
+    //   // }
+    // } else {
+    //   console.log("UserID not provided in handshake query");
+    // }
 
     socket.on("disconnect", () => {
       disconnect(socket);
